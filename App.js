8d7eea3bfb29f7c6c19debb6fda34e202b2a5992
Henry-kk <wxiao1214@gmail.com>
2020-06-18 17:51:44 +0800

    start project

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, { PureComponent } from 'react';
+import { StatusBar, StyleSheet, View } from 'react-native';
+import Entities from'./src/entities';
+import {GameEngine} from 'react-native-game-engine';
+
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      running: true,
+    };
+    this.gameEngine = null;
+    console.disableYellowBox = true;
+  }
+
+  render() {
+    return (
+      <View style = {styles.container}>
+        <GameEngiine
+          ref = {ref => {
+            this.gameEngine = ref;
+          }}
+          style = {styles.gameContainer}
+          entities = {Entities()}
+          running = {this.state.running} >
+          <StatusBar hidden = {true} />
+        </GameEngiine>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container : {
+    flex : 1,
+    backgroundColor : '#000'
+  },
+  gameContainer : {
+    position : 'absolute',
+    top : 0,
+    bottom : 0,
+    left : 0,
+    right : 0
+  },
+});
